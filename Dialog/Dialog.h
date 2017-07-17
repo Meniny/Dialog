890e806d7ff77de85869a57a55ce4d643f30b170
Meniny <meniny@qq.com>
2017-07-17 14:38:44 +0800

    .h

---
+++ Dialog/Dialog.h
@@ -0,0 +1,20 @@
+//
+//  Dialog.h
+//  Dialog
+//
+//  Created by Meniny on 2016-12-11.
+//  Copyright © 2016 Meniny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKIt.h>
+
+//! Project version number for Dialog.
+FOUNDATION_EXPORT double DialogVersionNumber;
+
+//! Project version string for Dialog.
+FOUNDATION_EXPORT const unsigned char DialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dialog/Dialog.h>
+
+
